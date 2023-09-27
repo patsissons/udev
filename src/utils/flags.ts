0e9a7e33221a2baf02c7bb17e5436b2d47910052
Pat Sissons <patrick@goldsky.com>
2023-09-27 15:48:39 -0700

    fixing issue with flag transormation

--- src/utils/flags.ts
+++ src/utils/flags.ts
@@ -1,13 +1,21 @@
+import kebabCase from 'lodash/kebabCase'
 import type { GlobalOptions } from '@/config'
 
 export function flagsForOptions<T extends GlobalOptions>(options: T) {
-  return Object.entries(options).reduce((list, [flag, value]) => {
-    return list.concat(value ? `--${flagName()}` : `--no-${flagName()}`)
+  return Object.entries(options).reduce((list, [key, value]) => {
+    const name = flagName()
+    const flag = value === false ? `--no-${name}` : `--${name}`
+
+    return list.concat(
+      typeof value === 'boolean'
+        ? flag
+        : `${flag}="${String(value).replace(/^["']/, '').replace(/["']$/, '')}"`
+    )
 
     function flagName() {
-      switch (flag) {
+      switch (key) {
         default:
-          return flag
+          return kebabCase(key)
       }
     }
   }, [] as string[])
